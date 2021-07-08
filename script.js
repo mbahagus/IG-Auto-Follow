e9f6f44924bc0c1ceebbae3e0ab99a68e261b81b
AgustD <agusdwisaputra159@gmail.com>
2021-07-08 21:36:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 setInterval(function () {
-    if () {
-        var area = document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ");
-        for (var i = 0; i < area.length; i++) {
-            if (area[i].innerHTML.includes("Ikuti")) {
-                area[i].click();
-                break;
-            }
+    var area = document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ");
+    for (var i = 0; i < area.length; i++) {
+        if (area[i].innerHTML.includes("Ikuti")) {
+            area[i].click();  
         }
     }
 }, 1000);
