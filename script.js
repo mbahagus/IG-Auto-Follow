b1b99006cc2a973faf18c35b2904934c2bb2d2d7
AgustD <agusdwisaputra159@gmail.com>
2021-07-08 21:14:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-/**
- * Auto Follow Akun Instagram (Bahasa Indonesia)
- * Untuk Bahasa Inggris Ganti Ikuti Menjadi Follow
- * https://www.github.com/mbahagus/IG-Auto-Follow
- */
- 
 setInterval(function () {
-    $disabled = false;
-    if (!$disabled) {
-        var area = document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ");
-        for (var i = 0; i < area.length; i++) {
-            if (area[i].innerHTML.includes("Ikuti")) {
-                area[i].click();
-                break;
-            }
+    var area = document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ");
+    for (var i = 0; i < area.length; i++) {
+        if (area[i].innerHTML.includes("Ikuti")) {
+            area[i].click();
+            break;
         }
     }
 }, 1000);
